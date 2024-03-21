68358ce0d8425e7cb089c5c3f69918133d8ea7b2
Daan Poron <daan@poron.be>
2024-03-21 13:20:16 +0100

    update types

--- fabric-ca-client/types/index.d.ts
+++ fabric-ca-client/types/index.d.ts
@@ -2,27 +2,46 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {ICryptoSuite, ICryptoKey, User} from 'fabric-common';
+import { ICryptoSuite, ICryptoKey, User } from "fabric-common";
 
 declare class FabricCAServices {
-	constructor(url: string | FabricCAServices.IFabricCAService,
-		tlsOptions?: FabricCAServices.TLSOptions, caName?: string, cryptoSuite?: ICryptoSuite, customHeaders?: Record<string, string>);
+	constructor(
+		url: string | FabricCAServices.IFabricCAService,
+		tlsOptions?: FabricCAServices.TLSOptions,
+		caName?: string,
+		cryptoSuite?: ICryptoSuite,
+		customHeaders?: Record<string, string>
+	);
 
 	getCaName(): string;
 
 	getCaInfo(registrar: User): Promise<FabricCAServices.ICAInfoResponse>;
 
-	register(req: FabricCAServices.IRegisterRequest, registrar: User): Promise<string>;
+	register(
+		req: FabricCAServices.IRegisterRequest,
+		registrar: User
+	): Promise<string>;
 
-	enroll(req: FabricCAServices.IEnrollmentRequest): Promise<FabricCAServices.IEnrollResponse>;
+	enroll(
+		req: FabricCAServices.IEnrollmentRequest
+	): Promise<FabricCAServices.IEnrollResponse>;
 
-	reenroll(currentUser: User, attrReqs: FabricCAServices.IAttributeRequest[]): Promise<FabricCAServices.IEnrollResponse>;
+	reenroll(
+		currentUser: User,
+		attrReqs: FabricCAServices.IAttributeRequest[]
+	): Promise<FabricCAServices.IEnrollResponse>;
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	revoke(request: FabricCAServices.IRevokeRequest, registrar: User): Promise<any>;
+	revoke(
+		request: FabricCAServices.IRevokeRequest,
+		registrar: User
+	): Promise<any>;
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	generateCRL(request: FabricCAServices.IRestriction, registrar: User): Promise<any>;
+	generateCRL(
+		request: FabricCAServices.IRestriction,
+		registrar: User
+	): Promise<any>;
 
 	newIdentityService(): FabricCAServices.IdentityService;
 
@@ -44,6 +63,7 @@ declare namespace FabricCAServices {
 		tlsOptions?: TLSOptions;
 		caName?: string;
 		cryptoSuite?: ICryptoSuite;
+		customHeaders?: Record<string, string>;
 	}
 
 	export interface IKeyValueAttribute {
@@ -134,44 +154,64 @@ declare namespace FabricCAServices {
 
 	// eslint-disable-next-line no-shadow
 	export enum HFCAIdentityType {
-		PEER = 'peer',
-		ORDERER = 'orderer',
-		CLIENT = 'client',
-		USER = 'user'
+		PEER = "peer",
+		ORDERER = "orderer",
+		CLIENT = "client",
+		USER = "user",
 	}
 
 	// eslint-disable-next-line no-shadow
 	export enum HFCAIdentityAttributes {
-		HFREGISTRARROLES = 'hf.Registrar.Roles',
-		HFREGISTRARDELEGATEROLES = 'hf.Registrar.DelegateRoles',
-		HFREGISTRARATTRIBUTES = 'hf.Registrar.Attributes',
-		HFINTERMEDIATECA = 'hf.IntermediateCA',
-		HFREVOKER = 'hf.Revoker',
-		HFAFFILIATIONMGR = 'hf.AffiliationMgr',
-		HFGENCRL = 'hf.GenCRL'
+		HFREGISTRARROLES = "hf.Registrar.Roles",
+		HFREGISTRARDELEGATEROLES = "hf.Registrar.DelegateRoles",
+		HFREGISTRARATTRIBUTES = "hf.Registrar.Attributes",
+		HFINTERMEDIATECA = "hf.IntermediateCA",
+		HFREVOKER = "hf.Revoker",
+		HFAFFILIATIONMGR = "hf.AffiliationMgr",
+		HFGENCRL = "hf.GenCRL",
 	}
 
 	export class AffiliationService {
-		create(req: IAffiliationRequest, registrar: User): Promise<IServiceResponse>;
+		create(
+			req: IAffiliationRequest,
+			registrar: User
+		): Promise<IServiceResponse>;
 
 		getOne(affiliation: string, registrar: User): Promise<IServiceResponse>;
 
 		getAll(registrar: User): Promise<IServiceResponse>;
 
-		delete(req: IAffiliationRequest, registrar: User): Promise<IServiceResponse>;
+		delete(
+			req: IAffiliationRequest,
+			registrar: User
+		): Promise<IServiceResponse>;
 
-		update(affiliation: string, req: IAffiliationRequest, registrar: User): Promise<IServiceResponse>;
+		update(
+			affiliation: string,
+			req: IAffiliationRequest,
+			registrar: User
+		): Promise<IServiceResponse>;
 	}
 
 	export class IdentityService {
 		create(req: IIdentityRequest, registrar: User): Promise<string>;
 
-		getOne(enrollmentID: string, registrar: User): Promise<IServiceResponse>;
+		getOne(
+			enrollmentID: string,
+			registrar: User
+		): Promise<IServiceResponse>;
 
 		getAll(registrar: User): Promise<IServiceResponse>;
 
-		delete(enrollmentID: string, registrar: User): Promise<IServiceResponse>;
+		delete(
+			enrollmentID: string,
+			registrar: User
+		): Promise<IServiceResponse>;
 
-		update(enrollmentID: string, req: IIdentityRequest, registrar: User): Promise<IServiceResponse>;
+		update(
+			enrollmentID: string,
+			req: IIdentityRequest,
+			registrar: User
+		): Promise<IServiceResponse>;
 	}
 }
